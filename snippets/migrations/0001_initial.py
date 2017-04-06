63d9225e14ab32116dc317d81cc7ae3ee03a76ce
Seunghun Lee <waydi1@gmail.com>
2017-04-07 01:33:49 +0900

    Fix typo

--- snippets/migrations/0001_initial.py
+++ snippets/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Snippets',
+            name='Snippet',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
