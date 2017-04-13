e65cbf7670e30b8e4ba090d14a399a36b88cbc1e
Seunghun Lee <waydi1@gmail.com>
2017-04-13 17:50:39 +0900

    Refactor import statement

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
